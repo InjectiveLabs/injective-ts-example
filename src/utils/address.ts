7d0b62e9a8990b9df581969df91d2eb89efca532
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2021-09-09 23:01:44 +0200

    feat: validate injective chain address

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -9,6 +9,16 @@ export const getInjectiveAddress = (address: string): string => {
   return bech32.encode("inj", bech32.toWords(addressBuffer));
 };
 
+export const getAddressFromInjectiveAddress = (address: string): string => {
+  if (address.startsWith("0x")) {
+    return address;
+  }
+
+  return `0x${Buffer.from(
+    bech32.fromWords(bech32.decode(address).words)
+  ).toString("hex")}`;
+};
+
 export const getAddressFromPrivateKey = (): string => {
   if (!PRIVATE_KEY) {
     throw new Error("You have to provide a PRIVATE_KEY in your .env");
@@ -31,5 +41,15 @@ export const validateAddress = (address: string): boolean => {
     return !!web3.utils.isAddress(address);
   } catch (e) {
     throw new Error(`Your address ${address} is not valid`);
+  }
+};
+
+export const validateInjectiveAddress = (injectiveAddress: string): boolean => {
+  const address = getAddressFromInjectiveAddress(injectiveAddress);
+
+  try {
+    return validateAddress(address);
+  } catch (e) {
+    throw new Error(`Your Injective address ${injectiveAddress} is not valid`);
   }
 };
