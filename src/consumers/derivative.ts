d944f6a2f1629c2b4d98798194e816ca126a6a31
Bojan Angjelkoski <bangjelkoskii@gmail.com>
2022-04-01 16:48:12 +0200

    fix: added comments

--- src/consumers/derivative.ts
+++ src/consumers/derivative.ts
@@ -3,9 +3,19 @@ import {
   DerivativeTransformer,
   DerivativeMarket,
 } from "@injectivelabs/derivatives-consumer";
-import { SENTRY_URL } from "../config";
+import { EXCHANGE_API_URL } from "../config";
 
-const derivativeConsumer = new DerivativeMarketConsumer(SENTRY_URL);
+/**
+ * You can use this function to get pre-defined endpoints based on the NETWORK you set
+ * in the .env
+ * const endpoints = getUrlEndpointForNetwork(NETWORK);
+ * const SENTRY_URL = endpoints.sentryGrpcApi;
+ * const EXCHANGE_API_URL = endpoints.exchangeApi;
+ *
+ * The exchangeApi endpoint is used to fetch data from the indexer API.
+ * the sentryGrpcApi endpoint is used to fetch data from the node directly.
+ */
+const derivativeConsumer = new DerivativeMarketConsumer(EXCHANGE_API_URL);
 
 export const fetchDerivativeMarkets = async (): Promise<DerivativeMarket[]> => {
   try {
